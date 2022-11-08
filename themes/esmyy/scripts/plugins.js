d9df228986e99e8e9d0c79ff30f4d6428482bbae
fengpeng <fengpeng213@gmail.com>
2022-11-09 00:56:41 +0800

    feat: 完成beta

--- themes/esmyy/scripts/plugins.js
+++ themes/esmyy/scripts/plugins.js
@@ -1,23 +1,9 @@
-const source = (path, cache, ext) => {
-  if (cache) {
-    const minFile = `${path}${
-      ext === ".js" || ext === "css" ? ".min" : ""
-    }${ext}`;
-    const jsdelivrCDN = hexo.config.jsdelivr;
-    return jsdelivrCDN.enable
-      ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@latest/${minFile}`
-      : `${minFile}?v=${version}`;
-  } else {
-    return path + ext;
-  }
-};
-
-hexo.extend.helper.register("theme_js", (path, cache) =>
-  source(path, cache, ".js")
-);
-hexo.extend.helper.register("theme_css", (path, cache) =>
-  source(path, cache, ".css")
-);
+hexo.extend.helper.register("img_url", (path, useCDN) => {
+  const jsdelivrCDN = hexo.config.jsdelivr;
+  return jsdelivrCDN.enable && useCDN
+    ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@master/${path}`
+    : path;
+});
 
 hexo.extend.helper.register("get_poster", (post) => {
   let poster = post.poster;
