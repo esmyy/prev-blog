a12cec376462086dd04ee89b0a89e0cf452ac9f3
fengpeng <fengpeng213@gmail.com>
2022-11-08 22:26:23 +0800

    feat: 添加插件

--- themes/esmyy/scripts/plugins.js
+++ themes/esmyy/scripts/plugins.js
@@ -19,7 +19,7 @@ hexo.extend.helper.register("theme_css", (path, cache) =>
   source(path, cache, ".css")
 );
 
-hexo.extend.helper.register("get_first_img", (post) => {
+hexo.extend.helper.register("get_poster", (post) => {
   let poster = post.poster;
   if (!post.poster) {
     const match = post.more.match(/<img src="([^\s]*)/);
@@ -27,4 +27,31 @@ hexo.extend.helper.register("get_first_img", (post) => {
   }
 
   return poster.replace(/["?\/]$/, "");
+});
+
+hexo.extend.helper.register("get_excerpt", (post) => {
+  return (
+    post.excerpt ||
+    post.more
+      .replace(/<[^>]*>/gi, " ")
+      .slice(0, 160)
+      .trim()
+      .replace(
+        /[\s=,.?!@#$%^&*()_+:"<>/\[\]\\`~——，。、《》？；’：“【】、{}|·！￥…（）-]*$/,
+        ""
+      )
+  );
+});
+
+hexo.extend.helper.register("get_last_of", (site, category) => {
+  const categories = site.categories.findOne((ca) => {
+    return ca.name === category;
+  });
+
+  let last = null;
+  if (categories.length) {
+    last = categories.posts.last();
+  }
+
+  return last;
 });
