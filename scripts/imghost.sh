4e5b15db0c8632be57e6f5a6a9410ef5d17ceb4c
esmyy <iamesmyy@gmail.com>
2023-02-01 10:44:06 +0800

    feat: 添加github pages部署

--- scripts/imghost.sh
+++ scripts/imghost.sh
@@ -2,12 +2,11 @@
 
 scp -r public/ .imghost
 rm .imghost/**/*.html
-
 scp -r .imghost/images ../imghost
 scp -r .imghost/2022 ../imghost
+rm -rf .imghost
 
 cd ../imghost
 git add .
 git commit -m "feat: update imgs"
 git push
-cd ../esmyy.com
