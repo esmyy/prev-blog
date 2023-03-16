1ba5de3bc51fee4cdbfc343e5104407706a77b95
esmyy <iamesmyy@gmail.com>
2023-03-16 18:01:26 +0800

    feat: docs

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/fengpeng/handbook/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/fengpeng/handbook/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/fengpeng/handbook/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/fengpeng/handbook/src/css/custom.css'),
+];
---
+++ docs/05-frameworks/_qiankun.md
@@ -0,0 +1,5 @@
+# 乾坤
+
+微前端方案
+
+<https://qiankun.umijs.org/>
