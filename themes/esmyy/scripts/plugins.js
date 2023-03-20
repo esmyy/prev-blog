a4e5959a3cecebc30a547e4ee6873968f55390bb
esmyy <iamesmyy@gmail.com>
2023-03-20 17:07:41 +0800

    feat: add valine

--- themes/esmyy/scripts/plugins.js
+++ themes/esmyy/scripts/plugins.js
@@ -2,14 +2,14 @@ function img_url(path, useCDN) {
   const jsdelivrCDN = hexo.config.jsdelivr;
   return jsdelivrCDN.enable && useCDN
     ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@main${path}`
-    : `/blog${path}`;
+    : `${path}`;
 }
 
 function css_url(path, useCDN) {
   const jsdelivrCDN = hexo.config.jsdelivr;
   return jsdelivrCDN.enable && useCDN
     ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@main${path}`
-    : `/blog${path}`;
+    : `${path}`;
 }
 
 function get_poster(post) {
@@ -22,7 +22,7 @@ function get_poster(post) {
   return poster.replace(/["?\/]$/, "").replace(/^\/blog/, "");
 }
 
-function get_excerpt(post) => {
+function get_excerpt(post) {
   return (
     post.excerpt ||
     post.more
@@ -53,12 +53,12 @@ hexo.extend.helper.register("get_poster", get_poster);
 hexo.extend.helper.register("get_excerpt", get_excerpt);
 hexo.extend.helper.register("get_last_of", get_last_of);
 
-;(() => {
-  if (typeof window !== 'undefined') {
-    window.hexoHelper.img_url = img_url
-    window.hexoHelper.css_url = css_url
-    window.hexoHelper.get_poster = get_poster
-    window.hexoHelper.get_excerpt = get_excerpt
-    window.hexoHelper.get_last_of = get_last_of
+(() => {
+  if (typeof window !== "undefined") {
+    window.hexoHelper.img_url = img_url;
+    window.hexoHelper.css_url = css_url;
+    window.hexoHelper.get_poster = get_poster;
+    window.hexoHelper.get_excerpt = get_excerpt;
+    window.hexoHelper.get_last_of = get_last_of;
   }
 })();
